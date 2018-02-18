82e4ee0063255885d4d1584e575382b2a99edcf5
bicarbonate <invalid.path@gmail.com>
2018-02-17 20:12:54 -0700

    Reddit change per: /u/auggielife

--- scrape_ind.py
+++ scrape_ind.py
@@ -1,11 +1,9 @@
 from bs4 import BeautifulSoup
-import urllib
 import requests
 url = requests.get('https://www.indeed.com/jobs?q=%28it+or+Network+or+Engineer+or+System+or+LAN+or+Administrator+or+Computer+or+Technology+or+Analyst%29+-nurse+-counselor+-driver+-burger+-dentist+-gyn+-deli+-therapist+-mechanic+-marine+-restaurant+-hvac+-mechanic+-child+-teacher+-instructor+-part+-hydraulic+-customer+-sales+-physician+-assistant+-car+-hispanic+-dog+-weld+-bartender+%2475%2C000&l=Michigan&radius=50&start=10')
-soup = BeautifulSoup(url.text, "html.parser")
-titles = soup.find_all('a')
-print(titles)
+req = url.text
+soup = BeautifulSoup(req, "html.parser")
+titles = soup.findAll("a", attrs={"data-tn-element": "jobTitle"})
+for item in titles:
+    print(item["title"])
 
-#titles_list = titles.find_all('title')
-#for titles in titles_list:
-#    print(titles.prettify())
